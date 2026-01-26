d566f731deaeb29d3410de93920e1f72ace3cb70
neuxdotdev <neuxdev1@gmail.com>
2026-01-26 15:55:27 +0700

    chore: add skeleton

---
+++ readme.txt
@@ -0,0 +1,4 @@
+neuxdotdev
+
+on this page this is a simple index repostory for neuxdotdev
+
---
+++ scripts/index.ts
@@ -0,0 +1,7 @@
+const a = 'hello';
+const b = 'world';
+const c = 'typescript';
+const d = 'javascript';
+const e = 'node';
+
+console.log(a, b, c, d, e);
